eddbd2f3b170b9fa9418d1de0baa6b2d79fa8754
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-05-03 00:02:49 +0800

    added script_loader to read functions from script

---
+++ blitz_ui.py
@@ -0,0 +1,11 @@
+
+
+def print_name():
+    print("nama")
+
+
+def add(a, b):
+    return a + b
+
+def print_add(a, b, longe=True):
+    print(a + b, longe)
--- pyblitzui/launcher.py
+++ pyblitzui/launcher.py
@@ -1,8 +1,23 @@
-import tkinter as tk
+from tkinter import Tk, Label, Entry
 
-root = tk.Tk()
+from script_loader import load_script
 
-w = tk.Label(root, text="Welcome to BlitzUI!")
-w.pack()
+FUNCTION_LIST = load_script("blitz_ui.py")
+
+root = Tk()
+
+w = Label(root, text="Welcome to BlitzUI!").grid(row=0)
+w = Label(root, text="Loaded file: blitz_ui.py").grid(row=1)
+
+y = 0
+for i in range(len(FUNCTION_LIST)):
+    function = FUNCTION_LIST[i]
+    w = Label(root, text=function["name"]).grid(row=2+y, column=0)
+    y += 1
+    for argname, argtype in function["args"].items():
+        w = Label(root, text=argname).grid(row=2+y, column=0)
+        g = Entry(root).grid(row=2+y, column=1)
+        y += 1
+
 
 root.mainloop()
