b36e7d8b0d21d0cd5ae98be5258c6b94765a6c69
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-05-05 23:55:32 +0800

    script_loader now ignores objects without `__code__`

--- blitz_ui.1.py
+++ blitz_ui.1.py
@@ -8,3 +8,11 @@ def new_print_name():
 def multiply(a, b):
     print("multiplying")
     return a * b
+
+
+class MyClass:
+    def __init__(self):
+        self.myname = "jeff"
+
+    def run(self):
+        print("running")
