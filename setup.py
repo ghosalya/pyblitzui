7479d08d15863c02e7adf18e9d843c841dc426b1
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-09-10 10:58:49 +0800

    fix setup.py to include the necessary static files

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(
     install_requires=[
         "flask==1.1.1",
     ],
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(exclude=["tests_*", "*example*"]),
+    include_package_data=True,
     entry_points="""
     [console_scripts]
     pyblitzui=pyblitzui.launcher:run
